eb3b08126e52dc3f1ed7be54e8b84c65cd4b1e6a
Mstislav Martynyuk <mmartynyuk@reunico.com>
2020-08-07 13:50:55 +0300

    initial commit

---
+++ src/main/java/ru/x5/camunda/access/example/ExampleApplication.java
@@ -0,0 +1,13 @@
+package ru.x5.camunda.access.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/x5/camunda/access/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.x5.camunda.access.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
